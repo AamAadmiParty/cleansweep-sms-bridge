17186f9c192fd9a748d4da470e4ac6b5cb6b13d8
Raghav Sharma <raghav_ftw@hotmail.com>
2015-10-16 01:19:08 +0530

    Added handle_request function to test web2sms app

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,11 @@
 from flask import Flask
 
+import os
+
 app = Flask(__name__)
+
+app.config.from_object('default_settings')
+if os.getenv('CLEANSWEEP-SMS_SETTINGS'):
+    app.config.from_envvar('CLEANSWEEP-SMS_SETTINGS')
+
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,6 +1,25 @@
 from app import app
+from flask import request, jsonify
 
 
 @app.route("/")
 def index():
     return "Hello, World!"
+
+
+@app.route("/sms-bridge/", methods=['GET'])
+def handle_request():
+    # Just ffs
+    if 'password' not in request.args or not request.args.get('password') or \
+            'phone' not in request.args or not request.args.get('phone') or \
+            'message' not in request.args or not request.args.get('message'):
+        return jsonify(), 400
+
+    password = request.args.get('password')
+    if password != app.config['PASSWORD']:
+        return jsonify(), 401
+
+    phone = request.args.get('phone')
+    message = request.args.get('message')
+    # TODO Break the message into parts and then do what's asked in some another function
+    return jsonify({'received': True, 'message': message})
